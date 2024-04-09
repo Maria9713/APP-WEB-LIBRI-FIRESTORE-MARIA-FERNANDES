0f1f49b0c47448400d32d59c247875728de3b1e1
Maria9713 <mvtfernandes2outlook.com>
2024-04-09 20:35:34 -0300

    Conexão firebase

---
+++ APP-WEB-LIBRI-MARIA-FERNANDES/app.js
@@ -0,0 +1,12 @@
+db.collection('libri-firestore')
+    .get()
+    .then(
+        (snapshot)=>{
+            // console.log(snapshot.docs)
+
+            snapshot.docs.forEach(doc => {
+                console.log(doc.data())
+
+            });
+        }
+    )
